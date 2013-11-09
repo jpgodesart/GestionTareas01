a9ca85266ae040a0b32ef55fe901bcd16e571dd9
Enrique <Enrique@Enrique-PC>
2013-11-09 17:32:49 +0100

    Inicio List incidencias

---
+++ src/main/java/net/daw/operation/IncidenciasList.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.daw.operation;
+
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import net.daw.bean.IncidenciasBean;
+import net.daw.dao.IncidenciasDao;
+import net.daw.helper.Contexto;
+
+/**
+ *
+ * @author Enrique
+ */
+public class IncidenciasList implements Operation{
+    
+     @Override
+    public Object execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Contexto oContexto = (Contexto) request.getAttribute("contexto");
+        oContexto.setVista("jsp/incidencias/list.jsp");
+        try {
+            IncidenciasDao oLenguajeDao = new IncidenciasDao(oContexto.getEnumTipoConexion());
+            Integer intPages = oLenguajeDao.getPages(oContexto.getNrpp(), oContexto.getHmFilter(), oContexto.getHmOrder());
+            if (oContexto.getPage() >= intPages) {
+                oContexto.setPage(intPages);
+            }
+            if (oContexto.getPage() < 1) {
+                oContexto.setPage(1);
+            }
+            ArrayList<IncidenciasBean> listado = (ArrayList<IncidenciasBean>) oLenguajeDao.getPage(oContexto.getNrpp(), oContexto.getPage(), oContexto.getHmFilter(), oContexto.getHmOrder());
+            String strUrl = "<a href=\"Controller?" + oContexto.getSerializedParamsExceptPage() + "&page=";            
+            ArrayList<String> vecindad = (ArrayList<String>) oLenguajeDao.getNeighborhood(strUrl, oContexto.getPage(), intPages, 2);
+            ArrayList<Object> a = new ArrayList<>();
+            a.add(listado);
+            a.add(vecindad);
+            return a;
+        } catch (Exception e) {
+            throw new ServletException("IncidenciasList1: View Error: " + e.getMessage());
+        }
+    }
+}
