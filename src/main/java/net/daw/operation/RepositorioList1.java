06a439af0d951061e5572e6ef9bf22146527879b
Ana Riquelme <al037793@s09-pc17>
2013-11-11 12:45:58 +0100

    Cambios en repositorio

--- src/main/java/net/daw/operation/RepositorioList1.java
+++ src/main/java/net/daw/operation/RepositorioList1.java
@@ -25,14 +25,14 @@ public class RepositorioList1 implements Operation {
         oContexto.setVista("jsp/repositorio/list.jsp");
         try {
             RepositorioDao oRepositorioDao = new RepositorioDao(oContexto.getEnumTipoConexion());
-            Integer intPages = oRepositorioDao.getPages(oContexto.getNrpp(), oContexto.getHmFilter(), oContexto.getHmOrder());
+            Integer intPages = oRepositorioDao.getPages(oContexto.getNrpp(), oContexto.getAlFilter(), oContexto.getHmOrder());
             if (oContexto.getPage() >= intPages) {
                 oContexto.setPage(intPages);
             }
             if (oContexto.getPage() < 1) {
                 oContexto.setPage(1);
             }
-            ArrayList<RepositorioBean> listado = (ArrayList<RepositorioBean>) oRepositorioDao.getPage(oContexto.getNrpp(), oContexto.getPage(), oContexto.getHmFilter(), oContexto.getHmOrder());
+            ArrayList<RepositorioBean> listado = oRepositorioDao.getPage(oContexto.getNrpp(), oContexto.getPage(), oContexto.getAlFilter(), oContexto.getHmOrder());
             String strUrl = "<a href=\"Controller?" + oContexto.getSerializedParamsExceptPage() + "&page=";            
             ArrayList<String> vecindad = (ArrayList<String>) oRepositorioDao.getNeighborhood(strUrl, oContexto.getPage(), intPages, 2);
             ArrayList<Object> a = new ArrayList<>();
