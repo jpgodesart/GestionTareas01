a72dbbce9a8816fcadd8228df25952be5913985a
al037721 <al037721@s09-pc18>
2013-11-12 10:40:10 +0100

    CuestionarioDao CuestionarioBean CuestionarioParam
    ..................... .. ... /´ /) 
    ...................... ..,../¯ ../ 
    ..................... ..../... ./ 
    .................... ..,/¯ ../ 
    .................... ./... ./ 
    .............../´¯/' ...'/´¯`•¸ 
    ............./'/.../... ./... ..../¨¯\ 
    ............('(...´(... ........,~/'...') 
    ..............\.......... ........\/..../ 
    ................\''.............. _.•´ 
    ..................\.............. 
    ....................\............

---
+++ src/main/java/net/daw/bean/CuestionarioBean.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author al037721
+ */
+package net.daw.bean;
+
+import java.util.Date;
+
+public class CuestionarioBean {
+
+    private Integer id = 0;
+    private String descripcion = "";
+    private Date fecha;
+    private Integer evaluacion = 0;
+    private Boolean activo;
+
+    public CuestionarioBean() {
+
+    }
+
+    public CuestionarioBean(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Integer getEvaluacion() {
+        return evaluacion;
+    }
+
+    public void setEvaluacion(Integer evaluacion) {
+        this.evaluacion = evaluacion;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+}
