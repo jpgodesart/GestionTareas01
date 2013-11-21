3a9d4ae87cc25c4d21ac47fbaf5debfe8480218e
Álvaro Crego <ereglion18@gmail.com>
2013-11-21 10:18:26 +0100

    Limpiado y generalizado parser

--- src/main/java/net/daw/helper/TextParser.java
+++ src/main/java/net/daw/helper/TextParser.java
@@ -16,12 +16,12 @@ public class TextParser {
     public static String toHtml(String text) throws Exception {
         ParserConverter oParserConverter = new ParserConverter();
         try {
-            text = oParserConverter.h6(text);
-            text = oParserConverter.h5(text);
-            text = oParserConverter.h4(text);
-            text = oParserConverter.h3(text);
-            text = oParserConverter.h2(text);
-            text = oParserConverter.h1(text);
+            text = oParserConverter.h(text,"======");
+            text = oParserConverter.h(text,"=====");
+            text = oParserConverter.h(text,"====");
+            text = oParserConverter.h(text,"===");
+            text = oParserConverter.h(text,"==");
+            text = oParserConverter.h(text,"=");
             text = oParserConverter.aExtern(text);
             text = oParserConverter.p(text);
         } catch (Exception e) {
