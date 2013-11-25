752a0c422f03f601f3ffb4ceb7cd71550a676bae
al037914 <al037914@s09-pc21>
2013-11-25 11:30:05 +0100

    añadidos permisos de usuarios en "opcion"

--- src/main/java/net/daw/operation/OpcionView2.java
+++ src/main/java/net/daw/operation/OpcionView2.java
@@ -7,6 +7,7 @@ package net.daw.operation;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import net.daw.bean.UsuarioBean;
 import net.daw.helper.Contexto;
 
 /**
@@ -18,11 +19,26 @@ public class OpcionView2 implements Operation {
     @Override
     public Object execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         Contexto oContexto = (Contexto) request.getAttribute("contexto");
-        oContexto.setVista("jsp/opcion/list.jsp");
-        oContexto.setClase("opcion");
-        oContexto.setMetodo("list");
-        oContexto.setFase("1");
-        OpcionList1 oOperacion = new OpcionList1();
-        return oOperacion.execute(request, response);
+
+        //Parte para saber el tipo de usuario
+        UsuarioBean oUsuarioBean;
+        oUsuarioBean = (UsuarioBean) request.getSession().getAttribute("usuarioBean");
+        java.lang.Enum tipoUsuario = oUsuarioBean.getTipoUsuario();
+
+        //Validación
+        if (tipoUsuario.equals(net.daw.helper.Enum.TipoUsuario.Profesor)
+                ||  tipoUsuario.equals(net.daw.helper.Enum.TipoUsuario.Alumno)  ) {
+
+            oContexto.setVista("jsp/opcion/list.jsp");
+            oContexto.setClase("opcion");
+            oContexto.setMetodo("list");
+            oContexto.setFase("1");
+            OpcionList1 oOperacion = new OpcionList1();
+            return oOperacion.execute(request, response);
+        } else {
+            //Mostramos el MENSAJE
+            oContexto.setVista("jsp/mensaje.jsp");
+            return "<span class=\"label label-important\">¡¡¡ No estás autorizado a entrar aquí !!!<span>";
+        }
     }
 }
