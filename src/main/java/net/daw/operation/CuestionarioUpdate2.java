4075b9edcce0a6aa672451f8bfb0d0bb900d840f
al037721 <al037721@s09-pc18>
2013-11-25 11:27:52 +0100

    Añadidos permisos de CUESTIONARIO

--- src/main/java/net/daw/operation/CuestionarioUpdate2.java
+++ src/main/java/net/daw/operation/CuestionarioUpdate2.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package net.daw.operation;
 
 /**
@@ -14,6 +13,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import net.daw.bean.CuestionarioBean;
+import net.daw.bean.UsuarioBean;
 import net.daw.dao.CuestionarioDao;
 import net.daw.helper.Contexto;
 import net.daw.parameter.CuestionarioParam;
@@ -23,17 +23,33 @@ public class CuestionarioUpdate2 implements Operation {
     @Override
     public Object execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         Contexto oContexto = (Contexto) request.getAttribute("contexto");
-        oContexto.setVista("jsp/mensaje.jsp");   
+
+        //Parte para saber el tipo de usuario
+        UsuarioBean oUsuarioBean;
+        oUsuarioBean = (UsuarioBean) request.getSession().getAttribute("usuarioBean");
+        java.lang.Enum tipoUsuario = oUsuarioBean.getTipoUsuario();
+        //
+
+        oContexto.setVista("jsp/mensaje.jsp");
         CuestionarioBean oCuestionarioBean = new CuestionarioBean();
         CuestionarioDao oCuestionarioDao = new CuestionarioDao(oContexto.getEnumTipoConexion());
         CuestionarioParam oCuestionarioParam = new CuestionarioParam(request);
         oCuestionarioBean = oCuestionarioParam.loadId(oCuestionarioBean);
         oCuestionarioBean = oCuestionarioParam.load(oCuestionarioBean);
-        try {
-            oCuestionarioDao.set(oCuestionarioBean);
-        } catch (Exception e) {
-            throw new ServletException("CuestionarioController: Update Error: Phase 2: " + e.getMessage());
+
+        //Validacion
+        if (tipoUsuario.equals(net.daw.helper.Enum.TipoUsuario.Profesor)) {
+            try {
+                oCuestionarioDao.set(oCuestionarioBean);
+            } catch (Exception e) {
+                throw new ServletException("CuestionarioController: Update Error: Phase 2: " + e.getMessage());
+            }
+            return "Se ha modificado la información del cuestionario con id=" + Integer.toString(oCuestionarioBean.getId());
+        } else {
+            //Mostramos el MENSAJE
+            oContexto.setVista("jsp/mensaje.jsp");
+            return "<span class=\"label label-important\">¡¡¡ No estás autorizado a entrar aquí !!!<span>";
+
         }
-        return "Se ha modificado la información del cuestionario con id=" + Integer.toString(oCuestionarioBean.getId());
     }
 }
--- src/main/java/net/daw/operation/CuestionarioView2.java
+++ src/main/java/net/daw/operation/CuestionarioView2.java
@@ -7,6 +7,7 @@ package net.daw.operation;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import net.daw.bean.UsuarioBean;
 import net.daw.helper.Contexto;
 
 /**
@@ -18,11 +19,27 @@ public class CuestionarioView2 implements Operation {
     @Override
     public Object execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         Contexto oContexto = (Contexto) request.getAttribute("contexto");
+
+        //Parte para saber el tipo de usuario
+        UsuarioBean oUsuarioBean;
+        oUsuarioBean = (UsuarioBean) request.getSession().getAttribute("usuarioBean");
+        java.lang.Enum tipoUsuario = oUsuarioBean.getTipoUsuario();
+        //
+
         oContexto.setVista("jsp/cuestionario/list.jsp");
         oContexto.setClase("cuestionario");
         oContexto.setMetodo("list");
         oContexto.setFase("1");
         CuestionarioList1 oOperacion = new CuestionarioList1();
-        return oOperacion.execute(request, response);
+
+        //Validacion
+        if (tipoUsuario.equals(net.daw.helper.Enum.TipoUsuario.Profesor)
+                || tipoUsuario.equals(net.daw.helper.Enum.TipoUsuario.Alumno)) {
+            return oOperacion.execute(request, response);
+        } else {
+            //Mostramos el MENSAJE
+            oContexto.setVista("jsp/mensaje.jsp");
+            return "<span class=\"label label-important\">¡¡¡ No estás autorizado a entrar aquí !!!<span>";
+        }
     }
 }
