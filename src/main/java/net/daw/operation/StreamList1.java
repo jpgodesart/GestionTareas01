b9e4b9703e5ab1af59a73812a01c9fefb85d9feb
al037914 <al037914@s09-pc21>
2013-11-28 11:41:56 +0100

    commit añadido de "stream"

---
+++ src/main/java/net/daw/operation/StreamList1.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.daw.operation;
+
+/**
+ *
+ * @author al037213
+ */
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import net.daw.bean.StreamBean;
+import net.daw.bean.UsuarioBean;
+import net.daw.dao.StreamDao;
+import net.daw.helper.Contexto;
+import net.daw.helper.Pagination;
+
+public class StreamList1 implements Operation {
+
+    @Override
+    public Object execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Contexto oContexto = (Contexto) request.getAttribute("contexto");
+
+        //Parte para saber el tipo de usuario
+        UsuarioBean oUsuarioBean;
+        oUsuarioBean = (UsuarioBean) request.getSession().getAttribute("usuarioBean");
+        java.lang.Enum tipoUsuario = oUsuarioBean.getTipoUsuario();
+        //
+
+        oContexto.setVista("jsp/stream/list.jsp");
+        //Validacion
+        if (tipoUsuario.equals(net.daw.helper.Enum.TipoUsuario.Profesor)
+                || tipoUsuario.equals(net.daw.helper.Enum.TipoUsuario.Alumno)) {
+
+            try {
+                StreamDao oStreamDAO = new StreamDao(oContexto.getEnumTipoConexion());
+                Integer intPages = oStreamDAO.getPages(oContexto.getNrpp(), oContexto.getAlFilter(), oContexto.getHmOrder());
+                if (oContexto.getPage() >= intPages) {
+                    oContexto.setPage(intPages);
+                }
+                if (oContexto.getPage() < 1) {
+                    oContexto.setPage(1);
+                }
+                ArrayList<StreamBean> listado = oStreamDAO.getPage(oContexto.getNrpp(), oContexto.getPage(), oContexto.getAlFilter(), oContexto.getHmOrder());
+                String strUrl = "<a href=\"Controller?" + oContexto.getSerializedParamsExceptPage() + "&page=";
+                //ArrayList<String> vecindad = oStreamDAO.getNeighborhood(strUrl, oContexto.getPage(), intPages, 2);
+                ArrayList<String> vecindad = Pagination.getButtonPad(strUrl, oContexto.getPage(), intPages, 2);
+                ArrayList<Object> a = new ArrayList<>();
+                a.add(listado);
+                a.add(vecindad);
+                return a;
+            } catch (Exception e) {
+                throw new ServletException("StreamList1: View Error: " + e.getMessage());
+            }
+        } else {
+            //Mostramos el MENSAJE
+            oContexto.setVista("jsp/mensaje.jsp");
+            return "<span class=\"label label-important\">¡¡¡ No estás autorizado a entrar aquí !!!<span>";
+        }
+    }
+}
+
